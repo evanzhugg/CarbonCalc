5d9e6cfe253432d536395c24210d72b6af46e762
evanzhu <evanzhu232@gmail.com>
2024-10-07 14:53:02 +0200

    Project Layout update

--- Data/Dump.py
+++ Data/Dump.py
@@ -2,6 +2,6 @@ import json
 def dump(data):
     json_object = json.dumps(data, indent = 4)
 
-    with open("data.json", "w") as outfile:
+    with open("../data.json", "w") as outfile:
         outfile.write(json_object)
 
